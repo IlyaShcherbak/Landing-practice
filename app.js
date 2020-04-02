0bc1fa7b86f505def8e2fc5d169c77dc1aa7044f
IlyaShcherbak <shcherbakilya@gmail.com>
2020-04-02 14:16:51 +0300

    Add modal window on "Learn more" button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//back to top button
 (function () {
     'use strict';
 
@@ -15,8 +16,8 @@
 
     function backToTop() {
         if (window.pageYOffset>0){
-            window.scrollBy(0, -80);
-            setTimeout(backToTop, 0);
+            window.scrollBy(0, -70);
+            setTimeout(backToTop, 10);
         }
     }
 
@@ -26,6 +27,54 @@
     goToBtn.addEventListener('click', backToTop);
 })();
 
+//modal window
+let modal = document.getElementById('modal-window');
+let btn = document.getElementById('learn-more-button');
+let close = document.getElementById('modal-close');
+const body = document.body;
 
+btn.onclick = function() {
+    body.style.overflowY = 'hidden';
+    modal.style.display = "block";
+};
 
+close.onclick = function() {
+    body.style.overflowY = 'auto';
+    modal.style.display = "none";
+};
 
+let mManagement = document.getElementById('modal-management');
+let mBranding = document.getElementById('modal-branding');
+let mDesign = document.getElementById('modal-design');
+let mAnimation = document.getElementById('modal-animation');
+
+let mManagementText = document.getElementById('modal-management-text');
+let mBrandingText = document.getElementById('modal-branding-text');
+let mDesignText = document.getElementById('modal-design-text');
+let mAnimationText = document.getElementById('modal-animation-text');
+
+mManagement.onclick = function() {
+    mManagementText.style.display = 'block';
+    mBrandingText.style.display = 'none';
+    mDesignText.style.display = 'none';
+    mAnimationText.style.display = 'none';
+};
+
+mBranding.onclick = function() {
+    mManagementText.style.display = 'none';
+    mBrandingText.style.display = 'block';
+    mDesignText.style.display = 'none';
+    mAnimationText.style.display = 'none';
+};
+mDesign.onclick = function() {
+    mManagementText.style.display = 'none';
+    mBrandingText.style.display = 'none';
+    mDesignText.style.display = 'block';
+    mAnimationText.style.display = 'none';
+};
+mAnimation.onclick = function() {
+    mManagementText.style.display = 'none';
+    mBrandingText.style.display = 'none';
+    mDesignText.style.display = 'none';
+    mAnimationText.style.display = 'block';
+};
